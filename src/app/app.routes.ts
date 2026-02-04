dcf4f04287ffb0bd7d70db18c6b888f838770c6f
dominictosku <dominic.tosku@gmail.com>
2026-02-04 11:27:30 +0100

    feat: add dashboard and settings component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,10 @@
 import { Routes } from '@angular/router';
 import { Inventory } from './inventory/inventory';
+import { Dashboard } from './dashboard/dashboard';
+import { Settings } from './settings/settings';
 
-export const routes: Routes = [{ path: 'inventory', component: Inventory }];
+export const routes: Routes = [
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    { path: 'dashboard', component: Dashboard },
+    { path: 'inventory', component: Inventory },
+    { path: 'settings', component: Settings }];
