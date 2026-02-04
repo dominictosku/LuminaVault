64e4e05fa059998918d9b69a92ddb2a646385a7f
dominictosku <dominic.tosku@gmail.com>
2026-02-04 15:50:33 +0100

    feat: modal for deletion of entry

--- mobile/src/app/inventory/inventory.ts
+++ mobile/src/app/inventory/inventory.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
+import { MessageService, ConfirmationService } from 'primeng/api';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+import { DialogModule } from 'primeng/dialog';
 
 interface Item {
   name: string;
@@ -11,12 +15,21 @@ interface Item {
 
 @Component({
   selector: 'app-inventory',
-  imports: [TableModule, ButtonModule, InputTextModule, FormsModule],
+  imports: [TableModule, ButtonModule, InputTextModule, FormsModule, ConfirmDialogModule, ToastModule, DialogModule],
+  providers: [MessageService, ConfirmationService],
   templateUrl: './inventory.html',
   styleUrl: './inventory.css',
 })
 export class Inventory {
+
+  confirmationService = inject(ConfirmationService);
+  messageService = inject(MessageService);
   items: Item[] = [];
+  visible: boolean = false;
+
+  showDialog() {
+    this.visible = true;
+  }
 
   newItem: Item = {
     name: '',
@@ -28,9 +41,38 @@ export class Inventory {
 
     this.items = [...this.items, { ...this.newItem }];
     this.newItem = { name: '', quantity: 0 };
+    this.visible = false;
   }
 
   removeItem(index: number) {
     this.items = this.items.filter((_, i) => i !== index);
+  }
+
+
+  confirm(event: Event, index: number) {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Do you want to delete this record?',
+      header: 'Danger Zone',
+      icon: 'pi pi-info-circle',
+      rejectLabel: 'Cancel',
+      rejectButtonProps: {
+        label: 'Cancel',
+        severity: 'secondary',
+        outlined: true
+      },
+      acceptButtonProps: {
+        label: 'Delete',
+        severity: 'danger'
+      },
+
+      accept: () => {
+        this.removeItem(index);
+        this.messageService.add({ severity: 'info', summary: 'Confirmed', detail: 'Record deleted' });
+      },
+      reject: () => {
+        this.messageService.add({ severity: 'error', summary: 'Rejected', detail: 'You have rejected' });
+      }
+    });
   }
 }
